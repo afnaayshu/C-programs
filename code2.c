2c1c9c9cfce195f215f16be4edbba26e076c0a31
Afna Ayshu Jaffin <76438840+afnaayshu@users.noreply.github.com>
2022-09-20 06:36:37 -1200

    C program that reverses characters btwn 2 indices in a string
     a C program that takes a string and two integers (n1, n2). Now reverse the sequence of characters of the string between n1 and n2.

--- code2.c
+++ code2.c
@@ -35,7 +35,7 @@ int main()
     strcpy(buff,string);
     n1 =n1-1;
     n2=n2-1;
-    for(i=0;i<length;i++)
+    for(i=0;i<=length;i++)
     {
        buff[n1] = string[n2];
        buff[n2] = string[n1];
